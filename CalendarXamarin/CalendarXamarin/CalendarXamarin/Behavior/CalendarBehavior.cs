e195b9d30e6d3a531b06510b6c5a72cd7614a842
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-24 12:44:14 +0530

    XAMARIN-36140 - Sample and screenshot added.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/Behavior/CalendarBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public class CalendarBehavior : Behavior<ContentPage>
+    {
+        SfCalendar calendar;
+        CalendarViewmodel viewModel;
+        private DateTime startDate;
+        private DateTime endDate;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.calendar = bindable.Content.FindByName<SfCalendar>("calendar");
+            this.viewModel = bindable.Content.FindByName<CalendarViewmodel>("viewModel");
+
+            this.WireEvents();
+        }
+        private void WireEvents()
+        {
+            this.calendar.SelectionChanged += Calendar_SelectionChanged;
+        }
+
+        private void Calendar_SelectionChanged(object sender, Syncfusion.SfCalendar.XForms.SelectionChangedEventArgs e)
+        {
+            if (this.calendar.SelectedRange != null)
+            {
+                startDate = viewModel.SelectedRange.StartDate;
+                endDate = viewModel.SelectedRange.EndDate;
+                App.Current.MainPage.DisplayAlert(startDate.ToString(), endDate.ToString(), "OK");
+            }
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.UnWireEvents();
+        }
+        private void UnWireEvents()
+        {
+            this.calendar.SelectionChanged += Calendar_SelectionChanged;
+        }
+    }
+}
+
---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/ViewModel/CalendarViewmodel.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarXamarin
+{
+    public class CalendarViewmodel
+    {
+        public SelectionRange SelectedRange { get; set; }
+        public CalendarViewmodel()
+        {
+          
+        }
+    }
+}
